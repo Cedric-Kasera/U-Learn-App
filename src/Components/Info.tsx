8645afd2b780887e49768fea337e4796d06dbd22
Omar Kasera <omarkasera50@gmail.com>
2024-11-10 16:25:05 +0300

    Update.

--- src/Components/Info.tsx
+++ src/Components/Info.tsx
@@ -1,7 +1,14 @@
 function Info() {
   return (
-    <section className="flex flex-row bg-[#f7fafc]">
-      
+    <section className="flex flex-col bg-[#ECF6FFff] py-6 md:py-16">
+      <span className="relative flex justify-center">
+        <div className="absolute inset-x-0 top-1/2 h-px -translate-y-1/2 bg-transparent bg-gradient-to-r from-transparent via-gray-500 to-transparent opacity-75"></div>
+
+        <span className="relative z-10 bg-[#ECF6FFff] px-6 font-sans text-2xl font-bold">We are There For You <span>🚀</span></span>
+      </span>
+      <div className="flex flex-col">
+
+      </div>
     </section>
   );
 }
